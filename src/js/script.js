8818fa02004e6b44fb96c9b2c3c584257850edc5
yuki.y <yuki198607070@gmail.com>
2024-04-17 10:08:28 +0900

    2024/4/17

--- src/js/script.js
+++ src/js/script.js
@@ -26,7 +26,17 @@ jQuery(function ($) {
   var campaignSwiper = new Swiper(".js-campaign-swiper", {
     clickable: true,
     loop: true,
+    slidesPerView: "auto",
     spaceBetween: 24,
+    autoplay: { delay: 2000 },
+    breakpoints: {
+      768: {
+        spaceBetween: 30,
+      },
+      1024: {
+        spaceBetween: 40,
+      },
+    },
     // 戻るボタンと進むボタンの設定
     navigation: {
       nextEl: ".swiper-button-next",
@@ -60,5 +70,31 @@ jQuery(function ($) {
         counter = 1;
       }
     });
+  });
+
+  let topBtn = $(".to-top");
+  topBtn.hide();
+
+  // ボタンの表示設定
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 70) {
+      // 指定px以上のスクロールでボタンを表示
+      topBtn.fadeIn();
+    } else {
+      // 画面が指定pxより上ならボタンを非表示
+      topBtn.fadeOut();
+    }
+  });
+
+  // ボタンをクリックしたらスクロールして上に戻る
+  topBtn.click(function () {
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      300,
+      "swing"
+    );
+    return false;
   });
 });
